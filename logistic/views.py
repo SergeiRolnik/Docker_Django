77971b9358578c68ac546a86be7dd88d25d29220
Сергей Рольник <forgedwheels@gmail.com>
2022-07-12 20:06:59 +0300

    views.py revised

--- logistic/views.py
+++ logistic/views.py
@@ -3,24 +3,15 @@ from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from rest_framework.pagination import PageNumberPagination
-
-class ProductViewSetPagination(PageNumberPagination):
-    page_size = 2
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['title', 'description']
-    pagination_class = ProductViewSetPagination
-
-class StockViewSetPagination(PageNumberPagination):
-    page_size = 3
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['address', 'products']
-    pagination_class = StockViewSetPagination
